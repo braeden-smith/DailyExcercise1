b04fe0037686cf78dcd408f9df48898b53fc6148
braeden-smith <34383327+braeden-smith@users.noreply.github.com>
2018-03-06 13:33:38 -0700

    Add files via upload

---
+++ DailyExcercise1.py
@@ -0,0 +1,12 @@
+#Braeden Smith - Python Daily Excercise - 2/08/18
+
+str1 = 'Hello'
+str2 = 'World'
+str3 = '!'
+print str1 + " " + str2 + str3 * 5
+
+name = input("What is your name: ")
+age = int(input("What is your age: "))
+year = 100 - int(age)
+year1 = int(year) + 2018
+print 'In', year1,'you will be 100 years old.'
